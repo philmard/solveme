9f5cbf5528fb319a940f18fa766c9a24a4a25ef6
anagnole <anagnole@outlook.com.gr>
2024-04-28 17:41:33 +0300

    initial commit

---
+++ microservice01/models/Credits.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const creditSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+const Credit = mongoose.model('Credit', creditSchema);
+
+module.exports = Credit;
